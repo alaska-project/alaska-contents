765fd465e91b820f02beb481139de00e1c52904e
Simone Sabba <Simone.Sabba@alpitourworld.it>
2019-07-11 20:29:47 +0200

    disabled

--- angular/projects/alaska-contents/src/lib/modules/content-editing/components/controls/changes-save-button/changes-save-button.component.ts
+++ angular/projects/alaska-contents/src/lib/modules/content-editing/components/controls/changes-save-button/changes-save-button.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ContentItem } from '@alaska-project/contents-core/dist/types/models/content-models';
 import { ContentsUpdateClient } from '../../../clients/contents-update.client';
 import { OperationsService } from '../../../services/operations/operations.service';
 import { ItemTrackerService } from '../../../../contents/services/item-tracker/item-tracker.service';
+import { SettingsService } from '../../../../contents/services/settings/settings.service';
+import { ContextService } from '../../../../contents/services/context/context.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'aly-changes-save-button',
   templateUrl: './changes-save-button.component.html',
   styleUrls: ['./changes-save-button.component.scss']
 })
-export class ChangesSaveButtonComponent implements OnInit {
+export class ChangesSaveButtonComponent implements OnInit, OnDestroy {
 
+  private pubTargetSub: Subscription;
+  private target: string;
+  
   constructor(
+    private settings: SettingsService,
+    private contextService: ContextService,
     private contentsUpdateClient: ContentsUpdateClient,
     private itemTracker: ItemTrackerService,
     private operation: OperationsService) { }
 
   ngOnInit() {
+    this.pubTargetSub = this.contextService.publishingTarget().subscribe(x => {
+      this.target = x;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.pubTargetSub.unsubscribe();
+  }
+
+  isDisabled() {
+    return this.target === this.settings.getSettings().webContentsTarget;
   }
 
   save() {
