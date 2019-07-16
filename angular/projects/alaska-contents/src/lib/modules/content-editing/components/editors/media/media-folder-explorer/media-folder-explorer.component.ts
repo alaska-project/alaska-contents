de335d9cbab79ae704374a5cfac0bcbd8893de04
Simone Sabba <Simone.Sabba@alpitourworld.it>
2019-07-17 01:51:52 +0200

    folders

--- angular/projects/alaska-contents/src/lib/modules/content-editing/components/editors/media/media-folder-explorer/media-folder-explorer.component.ts
+++ angular/projects/alaska-contents/src/lib/modules/content-editing/components/editors/media/media-folder-explorer/media-folder-explorer.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MediaFolderService } from '../../../../services/media-folders/media-folder.service';
+import { Subscription } from 'rxjs';
+import { MediaFolder } from '../../../../clients/media-library.clients';
 
 @Component({
   selector: 'aly-media-folder-explorer',
   templateUrl: './media-folder-explorer.component.html',
   styleUrls: ['./media-folder-explorer.component.scss']
 })
-export class MediaFolderExplorerComponent implements OnInit {
+export class MediaFolderExplorerComponent implements OnInit, OnDestroy {
 
-  constructor() { 
+  private folderSubscription: Subscription;
+  selectedFolder: MediaFolder;
+  
+  constructor(private mediaFolderService: MediaFolderService) { 
   }
 
   ngOnInit() {
+    this.folderSubscription = this.mediaFolderService.folderSelected().subscribe(x => {
+      if (x) {
+        this.selectedFolder = x.folder;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.folderSubscription.unsubscribe();
   }
 }
