9b1cda5e41f6e68c3c9b0c13fb77e3675af5d628
Eugenio Trabia <Eugenio.Trabia@alpitourworld.it>
2019-12-05 09:41:16 +0100

    last commit

--- core/src/components/fields/media-field-editor/media-field-editor-component.tsx
+++ core/src/components/fields/media-field-editor/media-field-editor-component.tsx
@@ -29,31 +29,30 @@ export class MediaFieldEditorComponent {
         }
         let url = this.field.value.url;
         if(!url){
-            return <img onLoad={()=> this.updateDuration(10)} onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class} src={Assets.noMediaContent} alt={this.field.value.alt}></img>
+            return <img onLoad={()=> this.updateState(10,"image")} onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class} src={Assets.noMediaContent} alt={this.field.value.alt}></img>
         } else {
             let type = getMediaType(url);
-            if(type === 'image'){
-                this.field.type = "image";
-                return <img onLoad={()=>this.updateDuration(10)} onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class} src={url} alt={this.field.value.alt}></img>;
-            } else if(type === 'video'){
-                this.field.type = "video";
+            if(type === 'image'){                
+                return <img onLoad={()=>this.updateState(10, "image")} onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class} src={url} alt={this.field.value.alt}></img>;
+            } else if(type === 'video'){                
                 let videoType = `video/${getExtension(url)}`
                 return (
                     <video muted 
-                    onLoadedMetaData={(e: any)=> this.updateDuration(Math.ceil(e.target.duration)) } 
+                    onLoadedMetaData={(e: any)=> this.updateState(Math.ceil(e.target.duration),"video") } 
                     onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class}>
                         <source src={url} type={videoType}></source>
                     </video>
                 )
             } else {
-                return <img onLoad={()=>this.updateDuration(10)} onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class} src={Assets.unsupportedMediaContent} alt={this.field.value.alt}></img>                
+                return <img onLoad={()=>this.updateState(10,"image")} onClick={() => this.edit.emit()} class={'image-editor ' + this.field.value.class} src={Assets.unsupportedMediaContent} alt={this.field.value.alt}></img>                
             }     
         }
     }    
-    updateDuration(time: number){
+    updateState(time: number, fieldType: string){
         if(!this.field.value.duration || this.field.value.duration === 0){
-            this.field.value.duration = time;
-            this.stateChanged.emit(); 
-        }                
+            this.field.value.duration = time;            
+        }           
+        this.field.type = fieldType;
+        this.stateChanged.emit();      
     }
 }
